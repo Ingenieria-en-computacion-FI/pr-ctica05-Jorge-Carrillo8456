a69b9cf537f54675de9df04c1db43216a01f504d
Jorge-Carrillo8456 <cuentaformaljorgecch@gmail.com>
2026-04-06 10:34:21 -0600

    Update pila.c

--- pila/pila.c
+++ pila/pila.c
@@ -1,49 +1,29 @@
 #include "pila.h"
+#include <stdio.h>
 
-Pila* pila_crear()
-{
-    /*
-    TODO
-    usar crear lista
-    */
+Pila* pila_crear() {
+    return lista_crear(); 
 }
 
-int pila_vacia(Pila* pila)
-{
-    /*
-    TODO
-    usar lista_vacia
-    */
+int pila_vacia(Pila* pila) {
+    return lista_vacia(pila);
 }
 
-void pila_push(Pila* pila, int dato)
-{
-    /*
-    TODO
-    usar lista_insertar_tail
-    */
+void pila_push(Pila* pila, int dato) {
+    lista_insertar_tail(pila, dato); 
 }
 
-int pila_pop(Pila* pila)
-{
-    /*
-    TODO
-    usar lista_eliminar_tail
-    */
-    return -1;
+int pila_pop(Pila* pila) {
+    return lista_eliminar_tail(pila); 
 }
 
-int pila_top(Pila* pila)
-{
-    /*
-    TODO
-    regresar dato del tail
-    */
-    return -1;
+int pila_top(Pila* pila) {
+    if (pila_vacia(pila)) return -1;
+    return pila->tail->dato; 
+    //Se supone que en una PILA el top es el head, pero como se esta solicitando que ingresemos 
+    //los datos apartir del tail, eso implica que el el top va a ser el tail
 }
 
-void pila_destruir(Pila* pila)
-{
-    // TODO
-    // Usar lista destruir
+void pila_destruir(Pila* pila) {
+    lista_destruir(pila);
 }
